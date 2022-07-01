a712e0a400724632e8f589e8c43d7f2663d210c8
axeljk <axeljkern@yahoo.com>
2022-07-01 16:45:23 -0400

    In process of adding functionality to table.

--- assets/src/script.js
+++ assets/src/script.js
@@ -12,18 +12,22 @@ function timerDisplay() {
 function dataSubmit() {
 	$("#addModal").modal("toggle");
 
-	let projectName = $("<td>").val($("#projectName").val());
-	console.log(projectName);
-	let projectType = $("<td>").val($("#projectType").val());
-	let projectDate = $("<td>").val($("#projectDate").val());
-	let projectHourly = $("<td>").val($("#projectPay").val());
-	let projectDays = $("<td>").val("Never");
-	let projectEarnings = $("<td>").val(projectHourly.val() * 100);
+
+
 	let project = $("<tr>");
 
 	$("#projectList tbody").append("<tr>").addClass("bg-dark text-light");
 	project = $("#projectList tbody").find("tr:last");
-	project.append(projectName);
+	let projectName = $("<td>").text($("#projectName").val()).appendTo(project);
+	let projectType = $("<td>").text($("#projectType").val()).appendTo(project);
+	let projectHourly = $("<td>").text($("#projectPay").val()).appendTo(project);
+	let projectDate = $("<td>").text($("#projectDate").val()).appendTo(project);
+	let daysPaid = Math.ceil((moment(projectDate.text()).unix() - moment().unix()) / 86400);
+	let projectDays = $("<td>").text(daysPaid).appendTo(project);
+	let projectEarnings = $("<td>").text(projectHourly.text() * 8 * daysPaid).appendTo(project);
+	let projectRemove = $("<td>").text("").appendTo(project);
+	console.log(project);
+//	project.append(projectName);
 	//project.find("td:last").after(projectType);
 	//project.find("td:last").after(projectHourly);
 /*
@@ -32,6 +36,5 @@ function dataSubmit() {
 	project.append(projectEarnings);
 	project.append(project); */
 
-	console.log(project);
 //	$("#projectList tbody").find("tr:last").after(project);
 }
