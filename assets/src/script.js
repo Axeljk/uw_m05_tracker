d463809f09d7c1538c97445b77584209ec67eefe
axeljk <axeljkern@yahoo.com>
2022-07-01 16:27:04 -0400

    In process of adding functionality to table.

--- assets/src/script.js
+++ assets/src/script.js
@@ -2,8 +2,36 @@ var timer;
 
 window.onload = () => {
 	timer = setInterval(timerDisplay, 1000);
+	$("#projectSubmit").click(dataSubmit);
 }
 
 function timerDisplay() {
 	$("#today").text(moment().format("MMM DD, YYYY [at] hh:mm:ss A"));
+}
+
+function dataSubmit() {
+	$("#addModal").modal("toggle");
+
+	let projectName = $("<td>").val($("#projectName").val());
+	console.log(projectName);
+	let projectType = $("<td>").val($("#projectType").val());
+	let projectDate = $("<td>").val($("#projectDate").val());
+	let projectHourly = $("<td>").val($("#projectPay").val());
+	let projectDays = $("<td>").val("Never");
+	let projectEarnings = $("<td>").val(projectHourly.val() * 100);
+	let project = $("<tr>");
+
+	$("#projectList tbody").append("<tr>").addClass("bg-dark text-light");
+	project = $("#projectList tbody").find("tr:last");
+	project.append(projectName);
+	//project.find("td:last").after(projectType);
+	//project.find("td:last").after(projectHourly);
+/*
+	project.append(projectDate);
+	project.append(projectDays);
+	project.append(projectEarnings);
+	project.append(project); */
+
+	console.log(project);
+//	$("#projectList tbody").find("tr:last").after(project);
 }
